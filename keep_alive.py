f6117d972ea580171bb4a68a0a774d52187215a6
VelvetaBot <108663991+VelvetaBot@users.noreply.github.com>
2026-01-16 22:16:08 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,9 +5,11 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "I am alive!"
+    return "I am alive"
 
 def run():
+    # Render assigns a port automatically via the PORT environment variable
+    # If not found, it defaults to 8080
     app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
